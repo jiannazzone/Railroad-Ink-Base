73b943da8a570c7eb48313136fb178ce5cb8ee9d
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2020-04-13 17:29:57 -0400

    Initial upload

---
+++ sketch.js
@@ -0,0 +1,112 @@
+//create empty array to represent each die
+//note that there are only two unique dice
+//the other two are duplicates
+let logo;
+let round = 0;
+let die1 = [];
+let die2;
+let die3 = [];
+let die4;
+
+// sizing and scaling
+let padding = 20;
+let diceY;
+let diceX = [];
+let diceSize;
+
+function preload() {
+  logo = loadImage('assets/logo.png');
+
+  //load images for first and second dice
+  die1.push(loadImage('assets/dice/0_0.jpg'));
+  die1.push(loadImage('assets/dice/0_1.png'));
+  die1.push(loadImage('assets/dice/0_2.png'));
+  die1.push(loadImage('assets/dice/0_3.png'));
+  die1.push(loadImage('assets/dice/0_4.png'));
+  die1.push(loadImage('assets/dice/0_5.png'));
+  die2 = die1;
+
+  //load images for third and fourth dice
+  die3.push(loadImage('assets/dice/1_0.png'));
+  die3.push(loadImage('assets/dice/1_1.png'));
+  die3.push(loadImage('assets/dice/1_2.png'));
+  die3.push(loadImage('assets/dice/1_3.png'));
+  die3.push(loadImage('assets/dice/1_4.jpg'));
+  die3.push(loadImage('assets/dice/1_5.jpg'));
+  die4 = die3;
+}
+
+function setup() {
+  rectMode(CENTER);
+  createCanvas(windowHeight, windowHeight);
+  makeScale();
+  decorations();
+  makeButton();
+  makeDice();
+}
+
+function makeScale() {
+  diceSize = (width - 7 * padding) / 4;
+  diceY = (25 / 32) * height;
+  let x = 2 * padding + diceSize / 2;
+  for (let i = 0; i < 4; i++) {
+    diceX.push(x);
+    x += padding + diceSize;
+  }
+}
+
+function decorations() {
+  background(42, 55, 118);
+  rect(width / 2, height / 2, width - padding, height - padding);
+  image(logo, padding / 2, padding / 2,
+    width - padding, 0.5 * (width - padding));
+}
+
+function makeButton() {
+  // button
+  fill(42, 55, 118);
+  stroke(236, 96, 80);
+  strokeWeight(5);
+  rect(width / 2, height / 2 + padding,
+    width - padding - 5, height / 16);
+
+  // label text
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  noStroke();
+  fill(255);
+  text("Click to roll the dice",
+    width / 2, height / 2 + padding,
+    width - padding - 5, height / 16)
+}
+
+function makeDice() {
+  for (let i = 0; i < 4; i++) {
+    stroke(0);
+    noFill();
+    square(diceX[i], diceY, diceSize, padding);
+  }
+}
+
+function mousePressed() {
+  if (mouseY >= height / 2 &&
+    mouseY <= (9 / 16) * height + 5) {
+    rollDice();
+  }
+}
+
+function rollDice() {
+  image(random(die1),
+    diceX[0] - diceSize / 2, diceY - diceSize / 2,
+    diceSize, diceSize);
+  image(random(die2),
+    diceX[1] - diceSize / 2, diceY - diceSize / 2,
+    diceSize, diceSize);
+  image(random(die3),
+    diceX[2] - diceSize / 2, diceY - diceSize / 2,
+    diceSize, diceSize);
+  image(random(die4),
+    diceX[3] - diceSize / 2, diceY - diceSize / 2,
+    diceSize, diceSize);
+  makeDice();
+}
